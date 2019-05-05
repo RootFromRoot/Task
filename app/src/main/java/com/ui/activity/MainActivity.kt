e4891d1c22e2d16fac6fd51f102c26b95585277e
rootcat <RootFromRoot>
2019-05-05 21:20:16 +0300

    initial commit

---
+++ app/src/main/java/com/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.app.task.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
